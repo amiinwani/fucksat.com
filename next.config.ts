a6cafd444c0801dc131ab4ddc581dcd977cc3eae
Arham Amin <132888838+arhaamwanii@users.noreply.github.com>
2025-08-31 20:05:27 +0530

    changes next js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://sat.cuirolearn.co/generate/khan-algebra',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
